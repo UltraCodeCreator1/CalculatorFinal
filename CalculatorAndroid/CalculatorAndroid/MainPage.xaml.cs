7886161fc7e279daadf40cec73b4398b578d7373
Lorenzo Mencaroni <lorenzo.menca12@outlook.it>
2018-09-28 23:22:42 +0200

    LastChange

--- CalculatorAndroid/CalculatorAndroid/MainPage.xaml.cs
+++ CalculatorAndroid/CalculatorAndroid/MainPage.xaml.cs
@@ -10,32 +10,56 @@ namespace CalculatorAndroid
     public partial class MainPage : ContentPage
     {
         int risultato;
+        bool check;
+        string puntatore;
         public MainPage()
         {
             InitializeComponent();
             risultato = 0;
+            check = false;
         }
 
         private void btn_soum_Clicked(object sender, EventArgs e)
         {
             try
             {
-                risultato = risultato + Convert.ToInt32(ent_num1.Text);
+                if(check == false)
+                {
+                    risultato = Convert.ToInt32(ent_num1.Text);
+                    check = true;
+                }
+                else
+                {
+                    risultato = risultato + Convert.ToInt32(ent_num1.Text);
+
+                }
                 ent_num1.Text = "";
                 lbl_risultato.Text = risultato.ToString();
+                puntatore = "+";
             }catch(Exception)
             {
                 lbl_risultato.Text = "ERRORE";
             }
+
         }
 
         private void btn_sub_Clicked(object sender, EventArgs e)
         {
             try
             {
-                risultato = risultato - Convert.ToInt32(ent_num1.Text);
+                if (check == false)
+                {
+                    risultato = Convert.ToInt32(ent_num1.Text);
+                    check = true;
+                }
+                else
+                {
+                    risultato = risultato - Convert.ToInt32(ent_num1.Text);
+
+                }
                 ent_num1.Text = "";
                 lbl_risultato.Text = risultato.ToString();
+                puntatore = "-";
             }catch(Exception)
             {
                 lbl_risultato.Text = "ERRORE";
@@ -46,9 +70,18 @@ namespace CalculatorAndroid
         {
             try
             {
-                risultato = risultato / Convert.ToInt32(ent_num1.Text);
+                if (check == false)
+                {
+                    risultato = Convert.ToInt32(ent_num1.Text);
+                    check = true;
+                }
+                else
+                {
+                    risultato = risultato / Convert.ToInt32(ent_num1.Text);
+                }
                 ent_num1.Text = "";
                 lbl_risultato.Text = risultato.ToString();
+                puntatore = "/";
             }catch(Exception)
             {
                 lbl_risultato.Text = "ERRORE";
@@ -58,9 +91,18 @@ namespace CalculatorAndroid
         private void btn_mol_Clicked(object sender, EventArgs e)
         {
             try
-            { 
-                risultato = risultato * Convert.ToInt32(ent_num1.Text);
+            {
+                if (check == false)
+                {
+                    risultato = Convert.ToInt32(ent_num1.Text);
+                    check = true;
+                }
+                else
+                {
+                    risultato = risultato * Convert.ToInt32(ent_num1.Text);
+                }
                 ent_num1.Text = "";
+                puntatore = "*";
                 lbl_risultato.Text = risultato.ToString();
             }catch(Exception)
             {
@@ -70,8 +112,23 @@ namespace CalculatorAndroid
 
         private void btn_risultato_Clicked(object sender, EventArgs e)
         {
+            try
+            {
+                if (puntatore == "+")
+                    risultato = risultato + Convert.ToInt32(ent_num1.Text);
+                if (puntatore == "-")
+                    risultato = risultato - Convert.ToInt32(ent_num1.Text);
+                if (puntatore == "/")
+                    risultato = risultato / Convert.ToInt32(ent_num1.Text);
+                if (puntatore == "*")
+                    risultato = risultato * Convert.ToInt32(ent_num1.Text);
+            }catch(Exception)
+            {
+                lbl_risultato.Text = "ERRORE";
+            }
             lbl_risultato.Text = risultato.ToString();
             ent_num1.Text = "";
+            check = false;
             risultato = 0;
         }
     }
